94c02331847625573dc7556847e473f0bff580c8
sammy17 <chathura.abeyrathne.lk@gmail.com>
2019-01-27 11:21:10 +0530

    Re-added all

---
+++ riscv-pk/build/payload/hello.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+
+int main(){
+
+
+	printf("Hello World !!!");
+
+	return 0;
+}
---
+++ riscv-pk/testbuild/elf.d
@@ -0,0 +1,25 @@
+elf.o: ../pk/elf.c ../pk/mmap.h ../machine/vm.h ../machine/encoding.h \
+ ../pk/syscall.h ../machine/encoding.h ../pk/file.h ../machine/mtrap.h \
+ ../pk/pk.h ../pk/boot.h ../machine/bits.h ../pk/elf.h
+
+../pk/mmap.h:
+
+../machine/vm.h:
+
+../machine/encoding.h:
+
+../pk/syscall.h:
+
+../machine/encoding.h:
+
+../pk/file.h:
+
+../machine/mtrap.h:
+
+../pk/pk.h:
+
+../pk/boot.h:
+
+../machine/bits.h:
+
+../pk/elf.h:
